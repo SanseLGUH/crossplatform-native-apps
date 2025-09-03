a5f04ad5ef124b24cfc32823361634e00714f41a
SanseL <sanselguh@gmail.com>
2025-09-03 19:26:40 +0700

    Update main.rs

--- nazzy_auto/src/main.rs
+++ nazzy_auto/src/main.rs
@@ -3,7 +3,7 @@ mod ui;
 
 use serde::{Serialize, Deserialize};
 
-pub const METADATA: &str = "http://178.250.187.252:4462/manifest/na_meta.json";
+pub const METADATA: &str = "http://178.250.187.252:4462/metadata/na_meta.json";
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct MiniGame {
